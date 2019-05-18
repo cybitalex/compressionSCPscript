6066ec0163f672e514b079e418cfb637f3a64925
Christopher Carrasco <CarrascoC81@gmail.com>
2019-05-17 21:02:30 -0400

    first python code

---
+++ compression.py
@@ -0,0 +1,15 @@
+import os
+import sys
+
+# where are we?
+cwd = os.getcwd()
+print ("1", cwd)
+
+# # go down
+# os.chdir("samples")
+# print ("2", os.getcwd())
+
+# # go back up
+# os.chdir(os.pardir)
+# print ("3", os.getcwd())
+
